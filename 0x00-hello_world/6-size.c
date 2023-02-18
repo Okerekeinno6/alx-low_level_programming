9316c8740165380ed1b30d2b64e7af679bcce69e
okerekeinn6 <okerekeinno6@gmail.com>
2023-02-18 09:10:07 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print sizeOf
+ *
+ *
+ * return:always return 0 when successful
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("size of char: %lu byte(s)\n", (unsigned long)  sizeof(a);
+	printf("size of int: %lu byte(s)\n", (unsigned long) sizeof(b);
+	printf("size of long int: %lu byte(s)\n", (unsigned long) sizeof(c);
+	printf("size of long long int: %lu byte(s)\n", (unsigned long) sizeof(d);
+	printf("size of float: %ul byte(s)\n", (unsigned long) sizeof(e);
+	return (0);
+}
