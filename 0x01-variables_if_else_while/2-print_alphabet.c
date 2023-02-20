152503932089fb74c18ecade96cde24386caa301
okerekeinn6 <okerekeinno6@gmail.com>
2023-02-20 07:33:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char lowerCase;
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+		putchar(lowerCase'\n');
+
+	return (0);
+}
