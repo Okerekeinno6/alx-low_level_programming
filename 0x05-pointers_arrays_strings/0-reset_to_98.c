05cea374486e518e6cd1340558ad6dc5a9936b77
okerekeinn6 <okerekeinno6@gmail.com>
2023-02-27 14:22:53 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Code by Okereke
+*  Return: 0 when successful
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
