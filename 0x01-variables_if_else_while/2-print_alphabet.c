0cecff5b85541ff1c55e8b21abaa58556e8e9390
okerekeinn6 <okerekeinno6@gmail.com>
2023-03-01 02:42:09 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
  */
-void print_alphabet_x10(void)
+int main(void)
 {
-	int count = 0;
 	char letter;
 
-	while (count++ <= 9)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
